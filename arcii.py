b37ad8e231cc147f600a6cc829df26585e83445e
Wanyu DU <duwanyu0706@163.com>
2018-07-06 14:15:20 +0800

    modify the structure

--- arcii.py
+++ arcii.py
@@ -7,38 +7,51 @@ Created on Wed Jun 13 14:08:24 2018
 
 import numpy as np
 import pandas as pd
-from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
-from keras.layers import Input, Embedding
+from keras.layers import Input, Embedding, Activation
 from keras.layers.core import Dense, Reshape, Flatten, Dropout
 from keras.layers.convolutional import Conv1D, Conv2D
 from keras.layers.pooling import MaxPooling2D
 from keras.layers.merge import concatenate
 from keras.models import Model, load_model
 from keras.callbacks import ModelCheckpoint, EarlyStopping, CSVLogger
+from keras.layers.normalization import BatchNormalization
 
-# load dataset function
-def get_texts(file_path, mode=None):
-    # read data
-    file = pd.DataFrame(columns=['label','q1','q2'])
-    f=open(file_path, 'r', encoding='utf8')
-    lines=f.readlines()
-    for i, line in enumerate(lines):
-        if i>0:
-            file.loc[i-1]=line.split('\t')
-    f.close()
-    q1, q2 = file['q1'], file['q2']
+# load dataset
+def get_ids(qids):
+    ids = []
+    for t_ in qids:
+        ids.append(t_)
+    return np.asarray(ids)
+
+def get_texts(file_path, question_path, mode=None):
+    qes = pd.read_csv(question_path, sep='	', dtype=str)
+    qes = qes.dropna()
+    file = pd.read_csv(file_path, sep=' ', dtype=str)
+    file = file.dropna()
+    q1id, q2id = file['q1'], file['q2']
+    id1s, id2s = get_ids(q1id), get_ids(q2id)
+    all_words = {}
+    for i in range(len(qes)):
+        all_words[qes.iloc[i,0]]=qes.iloc[i,1]
     texts1 = []
     texts2 = []
-    for sent in q1:
-        texts1.append(sent)
-    for sent in q2:
-        texts2.append(sent)
-    if mode=='train':
+    for t_ in id1s:
+        texts1.append(all_words[t_].split(' '))
+    for t_ in id2s:
+        texts2.append(all_words[t_].split(' '))
+    if mode=='test':
+        return texts1, texts2
+    else:
         labels=list(file['label'])
         return np.array(labels), texts1, texts2
-    else:
-        return texts1, texts2
+
+def make_submission(predict_prob, sub_path):
+    with open(sub_path, 'w') as file:
+        file.write(str('y_pre') + '\n')
+        for line in predict_prob:
+            file.write(str(line[0]) + '\n')
+    file.close()
   
     
     
@@ -48,56 +61,41 @@ text2_maxlen=50
 filters_1d=text2_maxlen
 kernel_size_1d=3
 num_conv2d_layers=2
-filters_2d=[128,64]
+filters_2d=[256,128]
 kernel_size_2d=[[3,3], [3,3]]
 mpool_size_2d=[[2,2], [2,2]]
-dropout_rate=0.5
+dropout_rate=0
 batch_size=128
 
-TRAIN_PATH = 'data/train.txt'
-TEST_PATH = 'data/test.txt'
-WORD_EMBED='data/word_embedding.txt'
-model_path='checkpoints/arcii.h5'
-log_path='checkpoints/log_arcii.txt'
+TRAIN_PATH = 'data/quora/relation_train.txt'
+VALID_PATH = 'data/quora/relation_valid.txt'
+TEST_PATH = 'data/quora/relation_test.txt'
+QUESTION_PATH = 'data/quora/corpus_preprocessed.txt'
+WORD_EMBED='data/quora/embed_glove_d50'
+model_path='checkpoints/arcii_512_128.h5'
+log_path='checkpoints/log_arcii_512_128.txt'
 
 print('Load files...')
-word_embedding = pd.read_csv(WORD_EMBED, header=None, sep=' ')
-word_embed={}
-for i in range(len(word_embedding)):
-    word_embed[word_embedding.iloc[i,0]]=list(word_embedding.iloc[i,1:])
-
-all_labels, all_texts1, all_texts2 = get_texts(TRAIN_PATH, 'train')
-test_texts1, test_texts2 = get_texts(TEST_PATH, 'test')
+train_labels, train_texts1, train_texts2 = get_texts(TRAIN_PATH, QUESTION_PATH, 'train')
+valid_labels, valid_texts1, valid_texts2 = get_texts(VALID_PATH, QUESTION_PATH, 'valid')
+test_texts1, test_texts2 = get_texts(TEST_PATH, QUESTION_PATH, 'test')
 
 print('Prepare word embedding...')
-all_texts=all_texts1+all_texts2+test_texts1+test_texts2
-# prepare tokenizer
-t=Tokenizer(lower=False)
-t.fit_on_texts(all_texts)
-vocab_size=len(t.word_index)+1
-# integer encode the docs
-encoded_texts1=t.texts_to_sequences(all_texts1)
-encoded_texts2=t.texts_to_sequences(all_texts2)
-encoded_test_texts1=t.texts_to_sequences(test_texts1)
-encoded_test_texts2=t.texts_to_sequences(test_texts2)
-# pad docs
-padded_texts1=pad_sequences(encoded_texts1, maxlen=text1_maxlen, padding='post')
-padded_texts2=pad_sequences(encoded_texts2, maxlen=text2_maxlen, padding='post')
-padded_test_texts1=pad_sequences(encoded_test_texts1, maxlen=text1_maxlen, padding='post')
-padded_test_texts2=pad_sequences(encoded_test_texts2, maxlen=text1_maxlen, padding='post')
+# pad the docs
+padded_train_texts1=pad_sequences(train_texts1, maxlen=text1_maxlen, padding='post')
+padded_train_texts2=pad_sequences(train_texts2, maxlen=text2_maxlen, padding='post')
+padded_valid_texts1=pad_sequences(valid_texts1, maxlen=text1_maxlen, padding='post')
+padded_valid_texts2=pad_sequences(valid_texts2, maxlen=text2_maxlen, padding='post')
+padded_test_texts1=pad_sequences(test_texts1, maxlen=text1_maxlen, padding='post')
+padded_test_texts2=pad_sequences(test_texts2, maxlen=text1_maxlen, padding='post')
 # create a weight matrix for words in training docs
-embedding_matrix=np.zeros((vocab_size, embed_size))
-for word, i in t.word_index.items():
-    if word in word_embed.keys():
-        embedding_vector=word_embed[word]
-    else:
-        embedding_vector=np.random.rand(1,embed_size)
-    embedding_matrix[i]=embedding_vector
+word_embedding = pd.read_csv(WORD_EMBED, header=None, sep=' ')
+embedding_matrix=word_embedding.iloc[:,1:]
+vocab_size=len(word_embedding)
 
 print('Split train and valid set...')
-sample_size=len(all_labels)*4//5
-train_labels, train_texts1, train_texts2 = all_labels[:sample_size], padded_texts1[:sample_size], padded_texts2[:sample_size]
-valid_labels, valid_texts1, valid_texts2 = all_labels[sample_size:], padded_texts1[sample_size:], padded_texts2[sample_size:]
+train_labels, train_texts1, train_texts2 = train_labels, padded_train_texts1, padded_train_texts2
+valid_labels, valid_texts1, valid_texts2 = valid_labels, padded_valid_texts1, padded_valid_texts2
 
 
 print('Build model...')
@@ -110,19 +108,22 @@ d_embed = embedding(doc)
 
 layer1_input=concatenate([q_embed, d_embed])
 
-layer1_conv1=Conv1D(filters=filters_1d, kernel_size=kernel_size_1d, padding='same', activation='relu')(layer1_input)
-layer1_reshaped=Reshape((text1_maxlen, text2_maxlen, -1))(layer1_conv1)
+layer1_conv=Conv1D(filters=filters_1d, kernel_size=kernel_size_1d, padding='same')(layer1_input)
+layer1_activation=Activation('relu')(layer1_conv)
+layer1_reshaped=Reshape((text1_maxlen, text2_maxlen, -1))(layer1_activation)
 z=MaxPooling2D(pool_size=(2,2))(layer1_reshaped)
 
 for i in range(num_conv2d_layers):
-    z=Conv2D(filters=filters_2d[i], kernel_size=kernel_size_2d[i], padding='same', activation='relu')(z)
+    z=Conv2D(filters=filters_2d[i], kernel_size=kernel_size_2d[i], padding='same')(z)
+    z=Activation('relu')(z)
     z=MaxPooling2D(pool_size=(mpool_size_2d[i][0], mpool_size_2d[i][1]))(z)
     
 pool1_flat=Flatten()(z)
 pool1_flat_drop=Dropout(rate=dropout_rate)(pool1_flat)
-mlp1=Dense(64, activation='relu')(pool1_flat_drop)
-mlp2=Dense(32, activation='relu')(mlp1)
-out=Dense(1, activation='sigmoid')(mlp2)
+pool1_norm=BatchNormalization()(pool1_flat_drop)
+mlp1=Dense(64)(pool1_norm)
+mlp1=Activation('relu')(mlp1)
+out=Dense(1, activation='sigmoidoid')(mlp1)
 
 model=Model(inputs=[query, doc], outputs=out)
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['acc'])
@@ -161,16 +162,14 @@ valid_gen=generator(valid_texts1, valid_texts2, valid_labels, batch_size=batch_s
 test_gen=test_generator(padded_test_texts1, padded_test_texts2, batch_size=1, min_index=0, max_index=len(test_texts1))
 
 print('Train classifier...')
-history=model.fit_generator(train_gen, epochs=20, steps_per_epoch=len(train_texts1)//batch_size,
+history=model.fit_generator(train_gen, epochs=10, steps_per_epoch=len(train_texts1)//batch_size,
                   validation_data=valid_gen, validation_steps=len(valid_texts1)//batch_size, verbose=1,
                   callbacks=[ModelCheckpoint(model_path, monitor='val_loss', mode='min', save_best_only=True), 
-#                             EarlyStopping(monitor='val_loss', patience=3), 
-                             CSVLogger(log_path)])
+                             EarlyStopping(monitor='val_loss', patience=3), CSVLogger(log_path)])
 
-print('Predict...')
-model=load_model(model_path)
-preds=model.predict_generator(test_gen, steps=len(test_texts1))
-
+#print('Predict...')
+#model=load_model(model_path)
+#preds=model.predict_generator(test_gen, steps=len(test_texts1))
 
 print('Plot validation accuracy and loss...')
 import matplotlib.pyplot as plt
